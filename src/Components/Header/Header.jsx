2231693706b4ea120e3a4185b733e46751303216
Slavik-Bilyk <Slavataschor@ukr.net>
2024-09-19 20:21:05 +0300

    add header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div >
+            <ul>
+                <li>Products</li>
+            </ul>
+    </div>
+  )
+}
+
+export default Header
