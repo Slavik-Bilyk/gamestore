abaa1536c91a41f367be0b745e4ec2fb582e7f0b
Slavik-Bilyk <Slavataschor@ukr.net>
2024-09-19 22:30:42 +0300

    add Navlink

--- src/Components/AppBar/AppBar.jsx
+++ src/Components/AppBar/AppBar.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
+import styles from './AppBar.module.css'
+import { SlBasket } from "react-icons/sl";
+import { RxAvatar } from "react-icons/rx";
 
 const AppBar = () => {
   return (
     <>
-      <nav>
-        <NavLink to='products'>Products</NavLink>
-        <NavLink to='onSale'>On Sale</NavLink>
-        <NavLink to='contactUs'>Contact Us</NavLink>
-        <NavLink to='logIn'>Log in</NavLink>
+     <div className={styles.bg}>
+     <nav className={styles.container}>
+        <div className={styles.navigation}>
+            <NavLink className={styles.navigationItem} to='products'>Products</NavLink>
+            <NavLink className={styles.navigationItem} to='onSale'>On Sale</NavLink>
+            <NavLink className={styles.navigationItem} to='contactUs'>Contact Us</NavLink>
+            
+            <NavLink className={styles.navigationItem} to='logIn'>
+                <div className={styles.iconText}>
+                    <RxAvatar className={styles.navIcons} size={25}/>
+                    Log In
+                </div>
+            </NavLink>
+
+            <SlBasket size={25} className={styles.navigationItem}/>
+        </div>
       </nav>
       <main>
         <Outlet/>
       </main>
+     </div>
     </>
   )
 }
