c812c90426ff32eac9ce6af83470494a4a1afdf1
Slavik-Bilyk <Slavataschor@ukr.net>
2024-09-20 22:50:12 +0300

    add element Weeks Deals

---
+++ src/Components/WeeksDeals/WeeksDeals.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styles from './WeeksDeals.module.css'
+
+const WeeksDeals = () => {
+
+  
+
+  return (
+    <div className={styles.bg}>
+
+       
+       <div className={styles.container}>
+
+            <div className={styles.bgImg}>
+
+            <div className={styles.bgImg2}></div>
+
+            <div className={styles.bgImg3}></div>
+
+            <div className={styles.block}>
+                <h3>THIS WEEK'S DEALS</h3>
+                <h2><span>10%</span><br/>off all games</h2>
+                <button>Shop Now</button>
+</div>
+
+
+
+</div>
+
+
+
+
+</div>
+       </div>
+
+  
+  )
+}
+
+export default WeeksDeals
--- src/Components/pages/Homepage/HomePage.jsx
+++ src/Components/pages/Homepage/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import BestSellers from '../../BestSellers/BestSellers'
 import Shop from '../../Shop/Shop'
+import WeeksDeals from '../../WeeksDeals/WeeksDeals'
 
 const HomePage = () => {
   return (
    <div>
     <BestSellers/>
     <Shop/>
+    <WeeksDeals/>
    </div>
   )
 }
