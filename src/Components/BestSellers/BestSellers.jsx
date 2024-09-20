b6bef39acbb0a93cffdd7a01d5f0f239a29dbac8
Slavik-Bilyk <Slavataschor@ukr.net>
2024-09-20 21:37:26 +0300

    add photo

--- src/Components/BestSellers/BestSellers.jsx
+++ src/Components/BestSellers/BestSellers.jsx
@@ -3,13 +3,22 @@ import styles from './BestSellers.module.css'
 import blockImg from '../../images/blockImg.png'
 import BestSellerCard from './BestSellerCard/BestSellerCard'
 import sellerCard from '../../images/sellerCard.png'
+import sellerCard2 from '../../images/sellerCard2.png'
+import sellerCard3 from '../../images/sellerCard3.png'
+import sellerCard4 from '../../images/sellerCard4.png'
+import sellerCard5 from '../../images/sellerCard5.png'
+import sellerCard6 from '../../images/sellerCard6.png'
+import sellerCard7 from '../../images/sellerCard7.png'
+import sellerCard8 from '../../images/sellerCard8.png'
+import sellerCard9 from '../../images/sellerCard9.png'
+import sellerCard10 from '../../images/sellerCard10.png'
 
 
 const BestSellers = () => {
 
 const bestSellersList = [
     {
-        img: sellerCard,
+        img: sellerCard2,
         name: 'Playbox XZ Gold Edition',
         price: '$1,000,000',
         newPrice: null,
@@ -23,56 +32,56 @@ const bestSellersList = [
         sale: false
     },
     {
-        img: sellerCard,
+        img: sellerCard3,
         name: 'Mach Gaming Chair',
         price: '$129.99',
         newPrice: '$116.99',
         sale: true
     },
     {
-        img: sellerCard,
+        img: sellerCard4,
         name: 'X-2 Wireless Mouse',
         price: '$24.99',
         newPrice: null,
         sale: false
     },
     {
-        img: sellerCard,
+        img: sellerCard5,
         name: 'Spartan Mechanical',
         price: '$44.99',
         newPrice: '$40.49',
         sale: true
     },
     {
-        img: sellerCard,
+        img: sellerCard6,
         name: 'Raptor',
         price: '$29.99',
         newPrice: '$26.99',
         sale: true
     },
     { 
-        img: sellerCard,
+        img: sellerCard7,
         name: 'Wave',
         price: '$29.99',
         newPrice: null,
         sale: false
     },
     {
-        img: sellerCard,
+        img: sellerCard8,
         name: 'Wave Gen RX',
         price: '$579.99',
         newPrice: null,
         sale: false
     },
     {
-        img: sellerCard,
+        img: sellerCard9,
         name: 'Ancient Souls',
         price: '$39.99',
         newPrice: null,
         sale: false
     }, 
     {
-        img: sellerCard,
+        img: sellerCard10,
         name: 'Chronosplit',
         price: '$39.99',
         newPrice: null,
